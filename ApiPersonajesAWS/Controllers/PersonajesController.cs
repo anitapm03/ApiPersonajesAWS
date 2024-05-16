7435c04bf3fa6c5932b75fa2cb9562a4cc666a27
anitapm03 <apereiramolinero@gmail.com>
2024-05-16 11:23:04 +0200

    Add project files.

---
+++ ApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,43 @@
+﻿using ApiPersonajesAWS.Models;
+using ApiPersonajesAWS.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPersonajesAWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonajesController : ControllerBase
+    {
+        private RepositoryPersonajes repo;
+
+        public PersonajesController(RepositoryPersonajes repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        //[Route("[action]")]
+        public async Task<ActionResult<List<Personaje>>> GetPersonajes()
+        {
+            return await this.repo.GetPersonajesAsync();
+        }
+
+        [HttpGet("{id}")]
+        //[Route("[action]/{id}")]
+        public async Task<ActionResult<Personaje>> FindPersonaje(int id)
+        {
+            return await this.repo.FindPersonajeAsync(id);
+        }
+
+        [HttpPost]
+        //[Route("[action]")]
+        public async Task<ActionResult> InsertarPersonaje
+            (Personaje personaje)
+        {
+            await this.repo.CreatePersonajeAsync
+                (personaje.Nombre, personaje.Imagen);
+            return Ok();
+        }
+    }
+}
---
+++ ApiPersonajesAWS/Program.cs
@@ -0,0 +1,51 @@
+using ApiPersonajesAWS.Data;
+using ApiPersonajesAWS.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string connectionString =
+    builder.Configuration.GetConnectionString("BBDD");
+
+builder.Services.AddTransient<RepositoryPersonajes>();
+
+builder.Services.AddDbContext<PersonajesContext>
+ (options => options.UseMySql
+    (connectionString, ServerVersion.AutoDetect(connectionString) 
+));
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen( options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "Api personajes AWS",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json",
+        "Api Personajes AWS");
+    options.RoutePrefix = "";
+});
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
